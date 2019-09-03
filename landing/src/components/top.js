ac6305832fc2c988737b33ac401eebddd07abca2
danilissadovski <danilissadovski@gmail.com>
2019-09-03 11:42:48 +0300

    fix top

--- landing/src/components/top.js
+++ landing/src/components/top.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Media } from 'reactstrap';
+import logo from "../images/logo.jpeg";
 
 const Top = () => {
     return (
-        <div className="container-fluid" style={{ padding: '.9rem' }}>
-            <div className="float-left col-sm-4">
-                <h1 className="SVALKA-Important-cla">SVALKA</h1>
-                <p className="text-style-1">Important claim here</p>
-            </div>
-            <div className="float-right py-2 row Shortcut1-Shortcut2">
-                <Button caret color="white" className="col-sm px-lg-5">Shortcut1</Button>
-                <Button caret color="white" className="col-sm px-lg-5">Shortcut2</Button>
-                <Button caret color="white" className="col-sm px-lg-5">Shortcut3</Button>
-                <Button color="primary" className='ml-1 col-sm px-lg-5'>Get Started</Button>
-            </div>
-        </div>
+        <Media>
+                <img src={logo} height = "64px" width = "64px" className="logo-fix"/>
+                    <div className="container-fluid" style={{ padding: '.9rem' }}>
+                        <div className="float-left col-sm-4 py-3">
+                            <h1 className="SVALKA-Important-cla text-fix">SVALKA</h1>
+                            <p className="text-style-1">Important claim here</p>
+                        </div>
+                        <div className="float-right py-2 row Shortcut1-Shortcut2">
+                            <Button caret color="white" className="col-sm px-lg-5">Shortcut1</Button>
+                            <Button caret color="white" className="col-sm px-lg-5">Shortcut2</Button>
+                            <Button caret color="white" className="col-sm px-lg-5">Shortcut3</Button>
+                            <Button color="primary" className='ml-1 col-sm px-lg-5'>Get Started</Button>
+                        </div>
+                    </div>
+        </Media>
     );
 };
 

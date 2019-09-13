b59949a076dd751ae50cda303d2f0fbc8f02b53d
dgladyshev <dmitry.gladishev@gmail.com>
2019-09-14 02:42:34 +0400

    Add viewport to enable mobile scaling

--- landing/src/App.js
+++ landing/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     return (
         <div>
             <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <style>{'body { background-color: #f08b77; }'}</style>
             </Helmet>
             <Top/>
